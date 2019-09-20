cbcfffb3d1bed350a610ceeacd23847c4a3a7d6e
Robson William <robson.william@seduc.ce.gov.br>
2019-09-20 18:40:15 -0300

    Initial Commit

---
+++ src/main/java/com/dtb/springreact/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.dtb.springreact.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { 
+			"classpath:/META-INF/resources/", 
+			"classpath:/resources/", 
+			"classpath:/static/", 
+			"classpath:/public/"
+			};
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS", "HEAD");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+	}
+}
